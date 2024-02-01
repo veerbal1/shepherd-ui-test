4b54c89f4bfe79aa1751f93318aeb5b750f0eaca
Veerbal Singh <Veerbalsingh1@gmail.com>
2024-02-01 17:11:21 +0530

    Fixed hydration error

--- components/dialog/index.tsx
+++ components/dialog/index.tsx
@@ -1,8 +1,19 @@
+'use client';
 import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import StyledDialogContent from './styled-dialog-content';
+import { useEffect, useState } from 'react';
 
 function UIDialog() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // To fix hydration issue
+  if (!mounted) return null;
+
   return (
     <div className="absolute bottom-0 right-0 m-16">
       <Dialog>
